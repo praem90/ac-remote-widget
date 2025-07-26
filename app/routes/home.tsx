4db500a38fe0fd47648a15528adf0640d3c7d3f9
Mohan Raj <praem1990@gmail.com>
2025-07-27 02:19:26 +0530

    Increase button sizes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -34,25 +34,25 @@ export default function Home() {
       </div>
       <div className="w-full flex flex-col flex-grow justify-around">
           <div className="flex justify-between">
-              <Button size="lg" variant="secondary" onClick={() => setState({...state, power: !state.power})} className={`${state.power ? 'bg-green-500 text-light-500 text-white' : 'text-blue-500'} hover:bg-blue-500/90 shadow-xl`}>
-                  <Power />
+              <Button size="lg" variant="secondary" onClick={() => setState({...state, power: !state.power})} className={`${state.power ? 'bg-green-500 text-light-500 text-white' : 'text-blue-500'} hover:bg-blue-500/90 shadow-xl has-[>svg]:p-5 h-auto`}>
+                  <Power className="size-6" />
               </Button>
-              <Button size="lg" variant="secondary" onClick={() => setState({...state, mode: 1})} disabled={!state.power} className={`text-blue-500 ${state.mode === 1 ? 'bg-blue-500 text-white hover:bg-blue-500/90': ''} shadow-xl`}>
-                  <Snowflake />
+              <Button size="lg" variant="secondary" onClick={() => setState({...state, mode: 1})} disabled={!state.power} className={`text-blue-500 ${state.mode === 1 ? 'bg-blue-500 text-white hover:bg-blue-500/90': ''} shadow-xl has-[>svg]:p-5 h-auto`}>
+                  <Snowflake className="size-6" />
               </Button>
-              <Button size="lg" variant="secondary" onClick={() => setState({...state, mode: 2})} disabled={!state.power} className={`text-blue-500 ${state.mode === 2 ? 'bg-blue-500 text-white hover:bg-blue-500/90': ''} shadow-xl`}>
-                  <Waves />
+              <Button size="lg" variant="secondary" onClick={() => setState({...state, mode: 2})} disabled={!state.power} className={`text-blue-500 ${state.mode === 2 ? 'bg-blue-500 text-white hover:bg-blue-500/90': ''} shadow-xl has-[>svg]:p-5 h-auto`}>
+                  <Waves className="size-6" />
               </Button>
-              <Button size="lg" variant="secondary" onClick={() => setState({...state, mode: 3})} disabled={!state.power} className={`text-blue-500 ${state.mode === 3 ? 'bg-blue-500 text-white hover:bg-blue-500/90': ''} shadow-xl`}>
-                  <Fan />
+              <Button size="lg" variant="secondary" onClick={() => setState({...state, mode: 3})} disabled={!state.power} className={`text-blue-500 ${state.mode === 3 ? 'bg-blue-500 text-white hover:bg-blue-500/90': ''} shadow-xl has-[>svg]:p-5 h-auto`}>
+                  <Fan className="size-6" />
               </Button>
           </div>
           <div className="flex justify-around">
-              <Button size="lg" variant="outline" onClick={() => setState({...state, temp: --state.temp})} disabled={state.temp === 16} className="shadow-xl">
-                  <ChevronDown />
+              <Button size="lg" variant="outline" onClick={() => setState({...state, temp: --state.temp})} disabled={state.temp === 16} className="shadow-xl has-[>svg]:p-5 h-auto">
+                  <ChevronDown className="size-10"/>
               </Button>
-              <Button size="lg" variant="outline" onClick={() => setState({...state, temp: ++state.temp})} disabled={state.temp === 36} className="shadow-xl">
-                  <ChevronUp />
+              <Button size="lg" variant="outline" onClick={() => setState({...state, temp: ++state.temp})} disabled={state.temp === 36} className="shadow-xl has-[>svg]:p-5 h-auto">
+                  <ChevronUp className="size-10" />
               </Button>
           </div>
           <div className="flex flex-col w-full h-full justify-center">
