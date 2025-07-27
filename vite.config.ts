4b8b038c09f9c44791eff337050a99e1afef67b7
Mohan Raj <praem1990@gmail.com>
2025-07-27 06:08:04 +0530

    Fix basename

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: '/ac-remote',
+  base: '/ac-remote/',
 });
